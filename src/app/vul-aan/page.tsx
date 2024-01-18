5ec9341bb21f14f309c0d3b13342eb5434fa1438
Aram Limpens <a.limpens@amsterdam.nl>
2024-01-18 17:01:07 +0100

    Show import of components

--- src/app/vul-aan/page.tsx
+++ src/app/vul-aan/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '~components'
+
 export default function VulAan() {
   return (
     <main>
       <h1>Vul aan</h1>
+      <Button variant="secondary">Hoi</Button>
     </main>
   )
 }
