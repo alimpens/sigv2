138c75d5b62400a69795cf0a91be6ce16a86029b
Aram Limpens <a.limpens@amsterdam.nl>
2024-01-18 16:24:58 +0100

    Setup directory structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-import Button from '../components/Button/Button'
+import { Button } from '~components'
 
 import styles from './page.module.css'
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,8 @@ import type { PropsWithChildren } from 'react'
 
 import styles from './Button.module.css'
 
-const Button = ({ children, variant = 'primary' }: PropsWithChildren<{ variant: 'primary' | 'secondary' }>) => (
+export const Button = ({ children, variant = 'primary' }: PropsWithChildren<{ variant: 'primary' | 'secondary' }>) => (
   <button type="button" className={styles[variant]}>
     {children}
   </button>
 )
-
-export default Button
